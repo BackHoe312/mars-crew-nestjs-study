592289970d3c65be73203e97b4a7c326b3e58cb8
Backhoe312 <idb591575@gmail.com>
2025-08-17 14:25:36 +0900

    feat: Contact Entity

---
+++ src/contact/domain/contact.entity.ts
@@ -0,0 +1,37 @@
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, Unique } from "typeorm";
+
+@Entity({name: 'tb_contacts'})
+@Unique(['phone'])
+export class Contact {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({
+        type: 'varchar',
+        length: 64,
+        nullable: false,
+        comment: '이름'
+    })
+    name: string;
+    
+    @Column({
+        type: 'varchar',
+        length: 32,
+        nullable: false,
+        comment: '전화번호'
+    })
+    phone: string;
+
+    @CreateDateColumn({
+        type: 'datetime',
+        default: () => 'CURRENT_TIMESTAMP',
+        nullable: false
+    })
+    createAt: Date;
+
+    @DeleteDateColumn({
+        type: 'datetime',
+        nullable: true
+    })
+    deleteAt: Date | null;
+}
