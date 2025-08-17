80915b6158f88ba10ea66b8a907c2c93d5f43145
Backhoe312 <idb591575@gmail.com>
2025-08-17 14:51:08 +0900

    fix: Add dotenv and useGlobalPipes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ContactModule } from './contact/contact.module';
+import { Contact } from './contact/domain/contact.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      // host: process.env.DB_HOST,
+      // port: +(process.env.DB_PORT ?? 3306),
+      // username: process.env.DB_USERNAME,
+      // password: process.env.DB_PASSWORD,
+      // database: process.env.DB_DATABASE,
+      host: 'localhost',
+      port: 3306,
+      username: 'ct_admin',
+      password: 'ct_admin',
+      database: 'contact_db',
+      entities: [Contact],
+      synchronize: true,
+    }),
+    ContactModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  dotenv.config();
+  
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }));
+
+  console.log("err: " + process.env.DB_HOST);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
