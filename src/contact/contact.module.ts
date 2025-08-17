8d40f34b6b4f70ff2b602695ff0c96e90b45919f
Backhoe312 <idb591575@gmail.com>
2025-08-17 14:29:10 +0900

    feat: Import TypeORM, CustomRepository

---
+++ src/contact/contact.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { ContactService } from './service/contact.service';
+import { ContactController } from './controller/contact.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Contact } from './domain/contact.entity';
+import { ContactRepository } from './repository/contact.repository';
+import { TypeOrmExModule } from 'src/common/typeorm-ex.module';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Contact]), TypeOrmExModule.forCustomRepository([ContactRepository])],
+  controllers: [ContactController],
+  providers: [ContactService],
+  exports: [TypeOrmModule]
+})
+export class ContactModule {}
