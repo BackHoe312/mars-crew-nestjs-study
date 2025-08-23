3c857efadba80cba57563ca82d29d7583c4051d1
Backhoe312 <idb591575@gmail.com>
2025-08-24 03:45:05 +0900

    feat: init User

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './service/user.service';
 import { UserController } from './controller/user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './domain/user.entity';
+import { TypeOrmExModule } from 'src/common/typeorm-ex.module';
+import { UserRepository } from './repository/user.repository';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    TypeOrmExModule.forCustomRepository([UserRepository]),
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
